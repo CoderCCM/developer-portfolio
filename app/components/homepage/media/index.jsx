5cb0ebfc7b636c3ba45363ebfcbe7666dc33665e
Connor Magnuson <ConnorM.9@outlook.com>
2025-05-21 13:50:55 -0500

    Add Media Mentions

---
+++ app/components/homepage/media/index.jsx
@@ -0,0 +1,42 @@
+// @flow strict
+import MediaCard from './media-card';
+import { mediaData as media_mentions } from "@/utils/data/media";
+
+function Media() {
+
+  return (
+    <div id='media' className="relative z-50 border-t my-12 lg:my-24 border-[#25213b]">
+      <div className="w-[100px] h-[100px] bg-violet-100 rounded-full absolute top-6 left-[42%] translate-x-1/2 filter blur-3xl  opacity-20"></div>
+
+      <div className="flex justify-center -translate-y-[1px]">
+        <div className="w-3/4">
+          <div className="h-[1px] bg-gradient-to-r from-transparent via-violet-500 to-transparent  w-full" />
+        </div>
+      </div>
+
+      <div className="flex justify-center my-5 lg:py-8">
+        <div className="flex  items-center">
+          <span className="w-24 h-[2px] bg-[#1a1443]"></span>
+          <span className="bg-[#1a1443] w-fit text-white p-2 px-5 text-xl rounded-md">
+            Media Mentions
+          </span>
+          <span className="w-24 h-[2px] bg-[#1a1443]"></span>
+        </div>
+      </div>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 md:gap-5 lg:gap-8 xl:gap-10">
+        {
+          media_mentions
+            .slice()
+            .sort((a, b) => (b.published_at - a.published_at))
+            .map((media, i) => (
+            media?.cover_image &&
+            <MediaCard media={media} key={i} />
+          ))
+        }
+      </div>
+    </div>
+  );
+};
+
+export default Media;
---
+++ utils/data/media.js
@@ -0,0 +1,35 @@
+import wifr_logo from "../../app/assets/svg/media/wifr_logo.svg";
+import rrstar_logo from "../../app/assets/svg/media/rrstar_logo.svg";
+import wrex_logo from "../../app/assets/svg/media/wrex_logo.svg";
+import sbd_logo from "../../app/assets/svg/media/sbd_logo.svg";
+
+export const mediaData = [
+    {
+        title: "Rockford Rotary Club Scholarship | WIFR",
+        description: "Received the 2024 Burdette Anderson Memorial Scholarship for community service.",
+        url: "https://www.wifr.com/2024/04/25/2-rockford-area-students-win-scholarships-community-service/",
+        cover_image: wifr_logo,
+        published_at: 1714066800000
+    },
+    {
+        title: "Rockford Christian High School Student Tests Among Best in the World | RR Star",
+        description: "Was one of the only 335 students worldwide to earn a perfect score on the 2021 AP Computer Science Principles Exam.",
+        url: "https://www.rrstar.com/story/news/2022/02/14/rockford-christian-high-school-student-illinois-tests-world-rankings/6723599001/",
+        cover_image: rrstar_logo,
+        published_at: 1644866340000
+    },
+    {
+        title: "Rockford Christian Student's Test Score Puts Him Among Smartest in the World | WREX",
+        description: "Was one of the only 335 students worldwide to earn a perfect score on the 2021 AP Computer Science Principles Exam.",
+        url: "https://www.wrex.com/news/featured-reports/rockford-christian-students-test-score-puts-him-among-smartest-in-the-world/article_bc39d8b2-9faa-11ec-ab21-076f4df08007.html",
+        cover_image: wrex_logo,
+        published_at: 1646818800000
+    },
+    {
+        title: "Four Youth Volunteers Will Receive 2023 Soap Box Derby Service Awards | ISBD",
+        description: "Was one of four youth nationwide to receive the 2023 President's Youth Volunteer Service Award.",
+        url: "https://www.soapboxderby.org/four-youth-volunteers-will-receive-2023-soap-box-derby-service-awards/",
+        cover_image: sbd_logo,
+        published_at: 1688539200000
+    }
+];
