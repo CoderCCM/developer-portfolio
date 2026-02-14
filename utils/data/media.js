bbd17a8eafa24c1cf9182206160c969f900abaa2
Connor Magnuson <ConnorM.9@outlook.com>
2026-02-13 21:38:49 -0600

    Add 2026 Chicago Auto Show

--- utils/data/media.js
+++ utils/data/media.js
@@ -2,6 +2,7 @@ import wifr_logo from "../../app/assets/svg/media/wifr_logo.svg";
 import rrstar_logo from "../../app/assets/svg/media/rrstar_logo.svg";
 import wrex_logo from "../../app/assets/svg/media/wrex_logo.svg";
 import sbd_logo from "../../app/assets/svg/media/sbd_logo.svg";
+import abc7_logo from "../../app/assets/svg/media/abc7_logo.svg";
 
 export const mediaData = [
     {
@@ -31,5 +32,12 @@ export const mediaData = [
         url: "https://www.soapboxderby.org/four-youth-volunteers-will-receive-2023-soap-box-derby-service-awards/",
         cover_image: sbd_logo,
         published_at: 1688539200000
+    },
+        {
+        title: "3 Illinois Universities at Chicago Auto Show | ABC 7 Chicago",
+        description: "Interviewed with ABC 7 Chicago to promote Illini Solar Car at the 2026 Chicago Auto Show.",
+        url: "https://abc7chicago.com/post/society-automotive-engineers-niu-uic-trying-recruit-engineering-students-2026-chicago-auto-show/18575673/",
+        cover_image: abc7_logo,
+        published_at: 1770681240000
     }
 ];
